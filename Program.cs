91472898a14969db560c48aaf0d44a3a76bbc167
its julii <nanji828@gmail.com>
2022-12-05 19:33:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < num)
+            {
+                int numNow = int.Parse(Console.ReadLine());
+                sum += numNow;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
